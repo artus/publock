2d18926040d76a8cf625ba6622669e269ec20a4f
artusvranken <artusvranken@gmail.com>
2017-12-09 14:07:38 +0100

    Removing connectivity for now, I have a deadline to meet and I don't want to waste time inventing a new peer2peer messaging protocol. I will implement it later, for now it will just be bidirectional relations between Publocks.

--- Publock.ts
+++ Publock.ts
@@ -13,10 +13,7 @@ export class Publock
     private _messageChain;
     private messageChainLoaded = false;
     
-    public connections : Map<string, IPeer>;
-    
-    public offeringConnection : IPeer;
-    public answeringConnection : IPeer;
+    public connections : Map<string, Publock>;
     
     public logging : boolean;
     
@@ -25,9 +22,7 @@ export class Publock
         this.id = (Publock.idCounter++).toString();
         this.logging = logging;
         this.messageChain = messageChain;
-        this.connections = new Map<string, IPeer>();
-        this.initialiseOfferingConnection();
-        this.initialiseAnsweringConnection();
+        this.connections = new Map<string, Publock>();
     }
     
     get messageChain() : MessageChain
@@ -42,77 +37,9 @@ export class Publock
     
     // Methods
     
-    initialiseOfferingConnection()
+    joinPublock(publock : Publock)
     {
-        let offeringPeer = new PeerStub(this.id);
-        
-        offeringPeer.on('connected', (peer) => {
-            this.connections.set(offeringPeer.id, offeringPeer);
-            this.log(this.id + ":" + offeringPeer.id + " connected to " + offeringPeer.otherPeer.publockId + ":" + offeringPeer.otherPeer.id);
-            
-            let connectionId = offeringPeer.id;
-            
-            this.initialiseOfferingConnection();
-        });
-                        
-        offeringPeer.on('disconnected', (peer) => {
-            this.connections.delete(offeringPeer.id);
-            this.log(this.id + " : " + offeringPeer.id + " disconnected from " + offeringPeer.otherPeer.publockId + " : " + offeringPeer.otherPeer.id);
-        });
-        
-        offeringPeer.on('data-received', (data) => this.dataReceived(offeringPeer.id, data));
-        offeringPeer.on('data-sent', (data) => {} );
-        
-        this.offeringConnection = offeringPeer;
-    }
-    
-    initialiseAnsweringConnection()
-    {
-        let answeringPeer = new PeerStub(this.id);
-        
-        answeringPeer.on('connected', (peer) => {
-            this.connections.set(answeringPeer.id, answeringPeer);
-            this.log(this.id + ":" + answeringPeer.id + " connected to " + answeringPeer.otherPeer.publockId + ":" + answeringPeer.otherPeer.id);
-            
-            this.initialiseAnsweringConnection();
-        });
-                        
-        answeringPeer.on('disconnected', (peer) => {
-            this.connections.delete(answeringPeer.id);
-            this.log(this.id + ":" + answeringPeer.id + " disconnected from " + answeringPeer.otherPeer.publockId + ":" + answeringPeer.id);
-        });
-        
-        answeringPeer.on('data-received', (data) => this.dataReceived(answeringPeer.id, data));
-        answeringPeer.on('data-sent', (data) => {} );
-        
-        this.answeringConnection = answeringPeer;
-    }
-    
-    answerConnection(offer : any)
-    {
-        this.answeringConnection.answerConnection(offer);
-    }
-    
-    connectToPeer(answer: any)
-    {
-        if (!this.isConnectedToPublock(answer.publockId))
-        this.offeringConnection.offerConnection(answer);
-    }
-        
-    dataReceived(connectionId: string, data : any)
-    {
-        //this.log(connectionId + ": " + data);
-        this.messageParser(connectionId, data);
-    }
-    
-    disconnectPeer(connectionId : string)
-    {
-        this.connections.get(connectionId).disconnect(undefined);
-    }
-    
-    sendData(connectionId : string, message : PeerMessage)
-    {
-        this.connections.get(connectionId).sendData(message);
+        // TODO
     }
     
     log(message : string)
@@ -120,98 +47,13 @@ export class Publock
         if (this.logging) console.log(message);
     }
     
-    messageParser(connectionId : string, message : PeerMessage)
-    {
-        switch (message.command)
-        {
-            case "connect-to-peer":
-                this.connectToPeer(message.data);
-            break;
-                
-            case "answer-peer":
-                this.answerConnection(message.data);
-            break;
-                
-            case "load-my-messagechain":
-                this.loadMessageChainFromPeer(message.data);
-            break;
-                
-            case "send-your-messagechain":
-                this.sendMessageChainToPeer(message.data);
-            break;
-                
-            case "my-connection-list":
-                this.connectToConnectionList(connectionId, message.data)
-            break;
-                
-            case "send-offer-to-publock":
-                this.sendOfferToPublock(message.data.publockId, message.data.offer);
-            break;
-                
-            case "my-answering-connection":
-            break;
-        }
-    }
-    
     isConnectedToPublock(publockId : string) : boolean
     {
         for (let connection of this.connections.values())
         {
-            if (connection.otherPeer.publockId == publockId) return true;
+            if (this.id == publockId || connection.id == publockId) return true;
         }
         return false;
     }
-    
-    getConnectionIdByPublockId(publockId : string)
-    {
-        for (let connection of this.connections.values())
-        {
-            if (connection.otherPeer.publockId == publockId) return connection.id;
-        }
-        return (-1).toString();
-    }
-    
-    getPublockIdByConnectionId(connectionId : string)
-    {
-        for (let connection of this.connections.values())
-        {
-            if (connection.id == connectionId) return connection.otherPeer.publockId;
-        }
-        return (-1).toString();
-    }
-    
-    connectToConnectionList(connectionId : string, publockList : Set<string>)
-    {
-        for (let publockId of publockList)
-        {
-            if (!this.isConnectedToPublock(publockId))
-            {
-                let message = new PeerMessage("send-offer-to-publock", { publockId : publockId, offer : this.offeringConnection} );
-                this.sendData(connectionId, message);
-            }
-        }
-    }
-    
-    sendOfferToPublock(publockId : string, offer : any)
-    {
-        if (this.isConnectedToPublock(publockId))
-        {
-            let message = new PeerMessage("answer-peer", offer);
-            let connectionId = this.getConnectionIdByPublockId(publockId);
-            this.sendData(connectionId, message);
-        }
-    }
-    
-    loadMessageChainFromPeer(messageChain : MessageChain)
-    {
-        if (!this.messageChainLoaded) this.messageChain = messageChain;
-        this.messageChainLoaded = true;
-    }
-    
-    sendMessageChainToPeer(connectionId : string)
-    {
-        let newPeerMessage = new PeerMessage("load-my-messagechain", this.messageChain);
-        
-        this.sendData(connectionId, newPeerMessage);
-    }
 }
+    
--- PublockTests.ts
+++ PublockTests.ts
@@ -7,62 +7,18 @@ function functionalityWorks(message : string, result : boolean)
 
 function creating_a_new_publock_works()
 {
-    let newPublock = new Publock();
     
-    let messgeChain = typeof newPublock.messageChain != 'undefined';
-    let connections = typeof newPublock.connections != 'undefined';
-    let offeringConnection = typeof newPublock.offeringConnection != 'undefined';
-    let answeringConnection = typeof newPublock.answerConnection != 'undefined';
-    
-    let result = messgeChain && connections && offeringConnection && answeringConnection;
-    
-    functionalityWorks("creating a new publock works.", result);
 }
 creating_a_new_publock_works();
 
 function connecting_to_other_publock_works()
 {
-    let p1 = new Publock(true);
-    let p2 = new Publock(true);
-    let p3 = new Publock(true);
-    let p4 = new Publock(true);
-    let p5 = new Publock(true);
-    let p6 = new Publock(true);
-    let p7 = new Publock(true);
     
-    p1.connectToPeer(p2.answeringConnection);
-    p2.connectToPeer(p3.answeringConnection);
-    p3.connectToPeer(p4.answeringConnection);
-    p4.connectToPeer(p5.answeringConnection);
-    p5.connectToPeer(p6.answeringConnection);
-    p6.connectToPeer(p7.answeringConnection);
-    
-    console.log(p1.connections.size);
-    console.log(p2.connections.size);
-    console.log(p3.connections.size);
-    console.log(p4.connections.size);
-    console.log(p5.connections.size);
-    console.log(p6.connections.size);
-    console.log(p7.connections.size);
-    
-    let result = p1.connections.size == 2 && p2.connections.size == 2 ;//&& p3.connections.size == 2;
-    
-    functionalityWorks("Connecting to other publocks works.", result);
 }
 connecting_to_other_publock_works();
 
 function disconnecting_other_publocks_works()
 {
-    let p1 = new Publock();
-    let p2 = new Publock();
     
-    let id = p1.offeringConnection.id;
-    
-    p1.connectToPeer(p2.answeringConnection);
-    p1.disconnectPeer(id);
-    
-    let result = p1.connections.size == 0 && p2.connections.size == 0;
-    
-    functionalityWorks("Disconnecting other publocks works.", result);
 }
 disconnecting_other_publocks_works();
